36b15eedca54dd5998f4e4cde6e20ee9dab59927
Punith B M <punith@designstring.com>
2023-08-13 01:18:22 +0530

    fix: updated meta image link

--- pages/[id].tsx
+++ pages/[id].tsx
@@ -46,7 +46,7 @@ export default function claim() {
     const uuid = router.asPath;
     return (
       <WagmiConfig config={config}>
-        <MetaHead title="Hey, Claim your Reward!!" description="Crypto Rewards Just in Link | Clink Safe" imageUrl="https://raw.githubusercontent.com/punithbm/eth-micropay-superhack/develop/public/assets/images/bg_card_topology.svg" urlEndpoint="" />
+        <MetaHead title="Hey, Claim your Reward!!" description="Crypto Rewards Just in Link | Clink Safe" imageUrl="https://raw.githubusercontent.com/punithbm/eth-micropay-superhack/develop/public/assets/images/meta.png" urlEndpoint="" />
         <ShareLink uuid={uuid} />
       </WagmiConfig>
     );
